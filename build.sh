4208e59d9dd00453a700416c975286a3f293cae4
yufu.deng <observeralone@gmail.com>
2023-01-04 17:50:44 +0800

    adjust

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
-APPDIR=`dirname $0`
-cd $APPDIR
+#!/usr/bin/env bash
+
+set -e
+
+pushd `dirname $0` > /dev/null
+SCRIPT_PATH=`pwd -P`
+cd $SCRIPT_PATH/
+
 dotnet restore zipkin4net.sln
 msbuild zipkin4net.sln
